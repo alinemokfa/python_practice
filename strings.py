809e7536bb01c034146b277252db913d1aee93d4
Aline <alinemokfa@hotmail.co.uk>
2018-08-15 23:05:16 +0100

    Strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+# apostrophe - double quote or escape it
+# "don't"
+# 'don\'t'
+
+# print raw, no newline
+print(r'C:\name')
+
+first_name = 'Aline'
+last_name = 'M'
+first_name + " " + last_name
+
+print(first_name[0])
+
+# slice sub string (first index included, 2nd excluded)
+print(first_name[0:2])
+
+print(len(first_name))
